e4d0e3c187b46efdac59a7a18fb851de10930dde
demetriomagatti <demetriomagatti@gmail.com>
2021-10-12 15:48:35 +0200

    Created structure to allow pip install

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bioinfo_tools',  
+    version='0.1',
+    author="Demetrio Magatti",
+    description="Package containing simple functions used for a first approach to the bioinformatics world",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
