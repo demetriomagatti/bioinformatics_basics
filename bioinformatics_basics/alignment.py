1661cde31e38dc70172a9915f9d6f2cdbd6e5df8
demetriomagatti <demetriomagatti@gmail.com>
2021-10-31 12:01:31 +0100

    major changes in ManhattanSolver; now working for maximum score evaluation; to add: saving taken steps to get to result.

--- bioinformatics_basics/alignment.py
+++ bioinformatics_basics/alignment.py
@@ -4,17 +4,48 @@ import pandas as pd
 
 class ManhattanSolver():
     def __init__(self,*args,**kwargs):
-        #Set default parameters
+        # Set default parameters
+        self.MaxScore = 10
         self.nrows = 10
         self.ncols = 10
-        #Update with provided parameters
+        self.highlight = True
+        # Update with provided parameters
         self.__dict__.update(kwargs)
-        #Actual initialization
-        self.MoveEast = int(np.random.rand(self.nrows,self.ncols-1))
-        self.MoveSouth = int(np.random.rand(self.nrows-1,self.ncols))
+        # Actual initialization
+        self.MoveEast = np.floor(self.MaxScore*np.random.rand(self.nrows,self.ncols-1))
+        self.MoveNord = np.floor(self.MaxScore*np.random.rand(self.nrows-1,self.ncols))
         self.Score = np.zeros((self.nrows,self.ncols))
     
+    
     def run(self):
+        # Fill first column
         for i in range(1,self.nrows):
-            Score[i,0] = 
-        return
+            self.Score[i,0] = self.Score[i-1,0] + self.MoveNord[i-1,0]
+        # Fill first row
+        for j in range(1,self.ncols):
+            self.Score[0,j] = self.Score[0,j-1] + self.MoveEast[0,j-1]
+        # Iteratively fill the Score Matrix
+        for i in range(1,self.nrows):
+            for j in range(1,self.ncols):
+                self.Score[i,j] = max(self.Score[i-1,j] + self.MoveNord[i-1,j],
+                                      self.Score[i,j-1] + self.MoveEast[i,j-1])
+        return self.Score,self.Score[self.nrows-1,self.ncols-1]
+    
+    
+    def visualize(self):
+        fig,ax = plt.subplots()
+        # Plain grid plot
+        x,y = np.meshgrid(np.arange(self.nrows),np.arange(self.ncols))
+        ax.plot(x,y,'o',markersize=5,color='black')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        # Steps and scores
+        for xpos in np.unique(x):
+            for ypos in np.unique(y)[:-1]:
+                ax.arrow(xpos,ypos+0.2,0,0.6,width=0.02,color='lightblue')
+                ax.text(xpos,ypos+0.5,str(np.transpose(self.MoveNord)[xpos,ypos]))
+        for xpos in np.unique(x)[:-1]:
+            for ypos in np.unique(y):
+                ax.arrow(xpos+0.2,ypos,0.6,0,width=0.02,color='lightblue')
+                ax.text(xpos+0.5,ypos,str(np.transpose(self.MoveEast)[xpos,ypos]))
+        return ax
